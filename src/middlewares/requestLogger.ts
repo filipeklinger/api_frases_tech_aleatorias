53737d16933c142ba6eec0c2bac60de85d2bb2f2
Filipe Klinger <filipeklinger@gmail.com>
2023-03-11 22:45:15 -0300

    chore: improve request logger

--- src/middlewares/requestLogger.ts
+++ src/middlewares/requestLogger.ts
@@ -1,6 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 
 export function requestLogger(req: Request, res: Response, next: NextFunction) {
-    console.log(new Date().toISOString(), " [", req.method, "] ", req.path, " ", res.statusCode);
+
+    function afterResponse() {
+        res.removeListener('finish', afterResponse);
+        // action after response
+        console.log(new Date().toISOString(), " [", req.method, "] ", req.path, " ", res.statusCode);
+    }
+
+    res.on('finish', afterResponse);
+
+    // action before request
+    // eventually calling `next()`
     next();
 }
